d3ce5f576270e02e8a9b7987135078334287831f
Sabeur Thabti <thabti.sabeur@gmail.com>
2018-02-17 03:24:41 +0000

    button

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import './style';
 import { Component } from 'preact';
-import { Button } from 'preact-fluid-test';
+import { Button } from 'preact-fluid-test'
 export default class App extends Component {
 	render() {
 		return (
 			<div>
+				<Button> Hello </Button>
 				<h1>Hello, World!</h1>
 			</div>
 		);
